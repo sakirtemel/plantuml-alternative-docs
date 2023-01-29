35eae65402ccb1c9035c6284f44f4248847b4447
Sakir Temel <sakirtemel@gmail.com>
2023-01-29 18:25:37 +0100

    Use custom html for rendering code block

--- _asciidoc/extensions/plantuml-source-block.js
+++ _asciidoc/extensions/plantuml-source-block.js
@@ -8,12 +8,12 @@ module.exports = function (registry) {
         self.process(function (parent, reader) {
             const lines = reader.getLines()
 
-            const codeBlock = self.createBlock(parent, 'listing', lines, { style: "source", language: "plantuml" }).convert()
+            const codeBlock = `<div class="listingblock"><div class="content"><pre class="highlightjs highlight"><code class="language-plantuml hljs" data-lang="plantuml">${lines.join("\n")}</code></pre></div></div>`
 
             const imagePath = plantumlEncoder.encode(lines.join("\n"))
             const imageBlock = `<img loading="lazy" src="https://plantuml.com/plantuml/pmg/${imagePath}"/>`
 
-            const html = `<div class="plantuml-diagram">${codeBlock} ${imageBlock}</div>`
+            const html = `<div class="paragraph plantuml-diagram">${codeBlock} ${imageBlock}</div>`
             return self.createBlock(parent, 'pass', html, {}, {})
         })
     })
