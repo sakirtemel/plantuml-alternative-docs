4b33dc78cc0970467438cffc787a1c28206f4a15
Sakir Temel <sakirtemel@users.noreply.github.com>
2023-01-29 14:55:53 +0100

    Update process script (#6)
    * Update process script
    
    * Fix typo
    
    * Fix typo
    
    * Process docs component of Antora
    
    * Update processing script to clean up before start
    
    * Fix throwing an error when no file exists
    
    * Fix undefined variable issue
    
    * Add non existing directory check
    
    * Process docs component of Antora
    
    * Remove wrong generated files

--- processAntoraDocsComponent.mjs
+++ processAntoraDocsComponent.mjs
@@ -1,17 +1,35 @@
 import fs from 'fs'
 
-const files = fs.readdirSync('docs_from_alphadoc/')
+const outputDirectory = 'docs/modules/ROOT/pages/'
+const sourceDirectory = 'docs_from_alphadoc/'
 
-for (let filename of files){
+// ensure the output directory exists
+if (!fs.existsSync(outputDirectory)){
+    fs.mkdirSync(outputDirectory, { recursive: true });
+}
+
+// clean up the pages before start
+let existingFiles = []
+try {
+  existingFiles = fs.readdirSync(outputDirectory)
+} catch (error) {
+  console.info(error)
+}
+for (const filename of existingFiles) {
+  fs.unlinkSync(`${outputDirectory}${filename}`)
+}
+
+// process each page
+for (let filename of fs.readdirSync(sourceDirectory)){
     if (filename === '.gitkeep'){
         continue
     }
 
-    let content = fs.readFileSync(`docs_from_alphadoc/${filename}`, 'utf8')
+    let content = fs.readFileSync(`${sourceDirectory}${filename}`, 'utf8')
 
-    // step1: use source code blocks for plantuml parts
+    // Step1: use source code blocks for plantuml parts
     content = content.replaceAll('<plantuml>', '[source, plantuml]\n----').replaceAll('</plantuml>', '----')
 
-    fs.writeFileSync(`docs/${filename}`, contentcontent)
+    fs.writeFileSync(`${outputDirectory}${filename}`, content)
     console.info(`Processed ${filename}`)
 }
